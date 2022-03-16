89199d8b5d85fd86b362a9d7be773fe1e30f2a4e
lehoamy17102000 <77613236+lehoamy17102000@users.noreply.github.com>
2022-03-16 12:26:18 +0700

    bh

---
+++ admin/panel_Bottom.php
@@ -0,0 +1,9 @@
+<br>
+<table border="1" bordercolor="0069A8" style="border-collapse: collapse" width="100%" cellspacing="0" cellpadding="0">
+	<tr>
+		<td height="20" align="center" bgcolor="#0069A8" class="midtitle">
+		<font style="font-size: 8.5pt" face="Tahoma">Copyright 
+		2021 @ Powered by </font>
+		<a href="http://shopMQ.com/"><font color="#ffffff" style="font-size: 8.5pt" face="Tahoma">ShopMQ.com</font></a></td>
+	</tr>
+</table>
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername     = "localhost";
+$username     = "root";
+$password     = "";
+$databasename = "MQ_shop";
+$visitorTimeout = 900; // =
+
+$MAXPAGE = 10;
+$multiLanguage = 1;//0 : single  ;  1 : multi
+
+$arrLanguage = array(
+	array('vn','Việt Nam'),
+	array('en','English')
+
+);
+
+?>
+
